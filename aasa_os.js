ead4b36ddd0ddbe5a996c0aa2b5a6e1eda16eacc
goaliebaba <64661344+goaliebaba@users.noreply.github.com>
2021-05-17 15:21:42 -0700

    Add files via upload

---
+++ aasa_os.js
@@ -0,0 +1,10 @@
+aasa_os
+alert(build_1Aasa_os);
+const backround = new png(arguments);
+backround; aasa_Os.png
+Node(run(aasa_Os.png ));
+console.log('this_was_made_for_programing_until_commercial_version_is_out")
+console.log(download visual studio code)
+(ww.microsoft.com/en-us/edge)
+console.log(chose_windows_or_mac)
+open(console);
